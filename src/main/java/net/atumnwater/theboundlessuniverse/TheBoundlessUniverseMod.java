f937248b358608828ccfa4d5b9afee3b8306c3b3
Caishangqi <2470159730@qq.com>
2023-12-19 13:06:32 +0800

    Fix some structural naming convention

--- src/main/java/net/atumnwater/theboundlessuniverse/TheBoundlessUniverseMod.java
+++ src/main/java/net/atumnwater/theboundlessuniverse/TheBoundlessUniverseMod.java
@@ -1,9 +1,11 @@
-package net.ZhongfanDong.The_Boundless_Universe_Mod;
+package net.atumnwater.theboundlessuniverse;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.entity.player.PlayerDestroyItemEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,16 +18,14 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(The_Boundless_Universe_Mod.MOD_ID)
-public class The_Boundless_Universe_Mod
-{
+@Mod(TheBoundlessUniverseMod.MOD_ID)
+public class TheBoundlessUniverseMod {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "the_boundless_universe_mod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public The_Boundless_Universe_Mod()
-    {
+    public TheBoundlessUniverseMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
@@ -35,34 +35,29 @@ public class The_Boundless_Universe_Mod
         modEventBus.addListener(this::addCreative);
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
     }
-
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
+    public void onServerStarting(ServerStartingEvent event) {
 
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
+    public static class ClientModEvents {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("Client SetUp");
         }
     }
 }
